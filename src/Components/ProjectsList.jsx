322b7d46e925d8de2ffeaadab8d4be8a112ea507
unknown <pathakprasoon37@gmail.com>
2024-12-10 01:19:35 +0530

    Added Todo project

--- src/Components/ProjectsList.jsx
+++ src/Components/ProjectsList.jsx
@@ -2,14 +2,30 @@
 import { forwardRef } from 'react'
 import Calendarlogo from '../assets/CalendarProject.png'
 import QuizLogo from '../assets/QuizAppsnap.png'
+import Todosnap from'../assets/TodoSNAP.png'
+
 const ProjectsList=forwardRef((props,ref)=> {
   return (
     <div className='projectList' ref={ref}>
+      <div className='project2 project'>
+        <div className='project2Description'>
+          <p>This is a todo-list that I made during my free time, it has simple functionalities 
+            such as delete, add todo and mark as completed.</p>
+          <a className='projectLink projectAnchor' target='_blank' href="https://todotaskmanage.netlify.app/">
+          ➡️➡️ Deployed Link
+          </a>
+        </div>
+        <div className='project2Snapshot'>
+          <a href={Todosnap} className='projectAnchor' target='_blank'>
+            <img src={Todosnap} className='QuizAppImg' alt="" />
+          </a>
+        </div>
+      </div>
       <div className='project1 project'>
         <div className='project1Description'>
-          <p>This is a To-Do-List made using ReactJs and API fetch...</p>
+          <p>This is a calendar app that I made using just ReactJs...</p>
           <a className='projectLink projectAnchor' href="https://github.com/PrasoonPathak12/Calendar-implication-using-Date-Object">
-          ➡️➡️Github Link to the project
+          ➡️➡️ Github Link to the project
           </a>
         </div>
         <div className='project1Snapshot'>
@@ -18,21 +34,18 @@ const ProjectsList=forwardRef((props,ref)=> {
       </div>
       <div className='project2 project'>
         <div className='project2Description'>
-          <p>          This is a quiz app made using ReactJS, it uses a 
+          <p>This is a quiz app made using ReactJS, it uses a 
           quiz API to fetch 10 random questions and the user
           has to answer these questions to get their score.</p>
           <a className='projectLink projectAnchor' href="https://github.com/PrasoonPathak12/Quiz-App">
-            ➡️➡️GitHub Link to the project
+            ➡️➡️ GitHub Link to the project
             </a>
         </div>
         <div className='project2Snapshot'>
           <a href={QuizLogo} className='projectAnchor' target='_blank'><img src={QuizLogo} alt="QuizAppLogo" className='QuizAppImg' /></a>
         </div>
       </div>
-      {/* <div className='project3 project'>
-        <div className='project3Description'></div>
-        <div className='project3Snapshot'></div>
-      </div>
+      {/* 
       <div className='project4 project'>
         <div className='project4Description'></div>
         <div className='project4Snapshot'></div>
